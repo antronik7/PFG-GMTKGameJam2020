283e1c7f4d10dc5b231b8623b986ee40408fa3d4
antronik7 <antoineberthiaumedutil@gmail.com>
2020-07-09 22:28:07 -0400

    Logo
    Added the scene with the logo transition.

---
+++ PFG-GMTKGameJam/Assets/LogoPFG/ChangeScene.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour {
+
+    public string sceneName;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    public void ChangeSceneEvent()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
